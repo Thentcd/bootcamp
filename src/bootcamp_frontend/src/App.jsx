8cb91dbb4cc2418273e0e1da57ece43ebb4cc619
szymongrodek <thentcd@gmail.com>
2024-06-24 19:09:05 +0200

    Final for day one

--- src/bootcamp_frontend/src/App.jsx
+++ src/bootcamp_frontend/src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react';
-import { bootcamp_backend } from 'declarations/bootcamp_backend';
-
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    bootcamp_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
+      <form action="#">
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
-      <section id="greeting">{greeting}</section>
+      <section id="greeting">Cześć</section>
     </main>
   );
 }
